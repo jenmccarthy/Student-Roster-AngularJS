25f95eb9ad81725e83abd1996e0726a99970e158
jenmccarthy <jennifernward@gmail.com>
2014-11-06 12:36:39 -0800

    add delete function to controller, and implement edit feature in html

--- js/StudentsController.js
+++ js/StudentsController.js
@@ -10,4 +10,8 @@ studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
     $scope.students.push({"name": $scope.studentName});
     $scope.studentName = null;
   };
+  $scope.deleteStudent = function(student) {
+  var index = $scope.students.indexOf(student)
+  $scope.students.splice(index, 1);
+  };
 });
