8973d81ec1bdacb0f7ff7c2deedd1bda6789dee4
jenmccarthy <jennifernward@gmail.com>
2014-11-06 10:05:33 -0800

    initial commit, app, index, and controller set up

---
+++ js/StudentsController.js
@@ -0,0 +1,9 @@
+studentRoster.controller('StudentsCtrl', function StudentsCtrl($scope) {
+  $scope.students = [
+    { "name":"Sam Schmidt" },
+    { "name":"Jessica Martin" },
+    { "name":"Sandy Smith" },
+    { "name":"Ryan Samuels" },
+    { "name":"Brentwood Davis" }
+  ]
+});
